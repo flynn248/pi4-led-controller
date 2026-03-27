b75210e59662b6583c5edd987f65f0464d2be2b2
Shane Flynn <sflynn248@gmail.com>
2026-03-27 13:49:13 -0500

    Update error messages

--- api/src/Led.Domain/LedStrips/ValueObjects/NameErrors.cs
+++ api/src/Led.Domain/LedStrips/ValueObjects/NameErrors.cs
@@ -8,5 +8,5 @@ public static class NameErrors
     private const string _baseErrorCode = "name";
     public const string EmptyErrorCode = $"{_baseErrorCode}.empty";
 
-    public static Error Empty => new Error("Hostname cannot be empty.").Validation(EmptyErrorCode);
+    public static Error Empty => new Error("Name cannot be empty").Validation(EmptyErrorCode);
 }
